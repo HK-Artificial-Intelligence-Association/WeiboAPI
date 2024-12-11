b550c98b547cc1a429a9c7a6434416de89758462
Wendaolee <leewendao@outlook.com>
2024-12-11 15:30:19 +0800

    Feat: 为 searchForUserUID 添加包统一导出

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { fetchUserLatestWeiboByUID,fetchUserLatestWeiboByUIDWithoutParsed } from "./utils/weibo.ts";
 import { analyazeWeiboInfoToOneSentenceSummary } from "./utils/analyzer.ts";
 import { parseCardInfosToWeiboInfos,type WeiboInfo } from "./utils/parser.ts";
+import { searchForUserUID } from "./utils/search.ts";
 import { type Card } from "./types/api/container/getIndex.ts";
 
 export {
@@ -9,5 +10,6 @@ export {
   analyazeWeiboInfoToOneSentenceSummary,
   parseCardInfosToWeiboInfos,
   type Card,
-  type WeiboInfo
+  type WeiboInfo,
+  searchForUserUID
 }
